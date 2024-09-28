f5f69d498be9d9a3e9f904810a9685b6776fb891
Danujaya Daham <91881550+danujaya2006@users.noreply.github.com>
2024-09-28 14:46:04 +0530

    Add files via upload
    DANUJAYA DAHAM

---
+++ 01_LED_BLINK/01_LED_BLINK.ino
@@ -0,0 +1,14 @@
+//DANUJAYA DAHAM 2k24 ...
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
